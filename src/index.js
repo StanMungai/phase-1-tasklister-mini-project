c0602dccb8042fe21e582714e3a98897cb139f83
stanford kangethe <stanfordkangethe16@gmail.com>
2023-05-26 06:25:12 +0300

    Halfway through the todo app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', createNewTask)
+
 });
+
+const createNewTask = (e) => {
+  // stop the form from trying to submit
+  e.preventDefault()
+  const inputTask = document.getElementById('new-task-description')
+  const tasks = document.getElementById('tasks')
+  const newTask = document.createElement('li')
+  const btn = document.createElement('button')
+  btn.textContent = 'x'
+
+  newTask.textContent = inputTask.value
+  newTask.appendChild(btn)
+  tasks.appendChild(newTask)
+
+  e.target.reset()
+}
