fe205949c52dd4d6e733bf98e7552fa619da7a00
stanford kangethe <stanfordkangethe16@gmail.com>
2023-05-27 11:49:51 +0300

    Dusted task lister miniproject

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.getElementById('create-task-form')
+  const inputTask = document.getElementById('new-task-description')
+  const tasks = document.getElementById('tasks')
 
   form.addEventListener('submit', createNewTask)
-
 });
 
 const createNewTask = (e) => {
   // stop the form from trying to submit
   e.preventDefault()
+  
   const inputTask = document.getElementById('new-task-description')
-  const tasks = document.getElementById('tasks')
   const newTask = document.createElement('li')
   const btn = document.createElement('button')
   btn.textContent = 'x'
+  btn.addEventListener('click', deleteButton)
 
   newTask.textContent = inputTask.value
   newTask.appendChild(btn)
   tasks.appendChild(newTask)
 
   e.target.reset()
+}
+
+const deleteButton = (e) => {
+  const task = document.querySelector('li')
+  task.remove()
 }
